4d1618cb528adf0b0f59d650850ba597b03f09fa
uvishere <uvishere@gmail.com>
2016-03-23 17:21:19 +0545

    Algorithm completed. Full Nepali Calendar with english date generated

--- newnepalidate.js
+++ newnepalidate.js
@@ -2,13 +2,61 @@ var setCalendar = function (currentDate) {
 	var tempFormattedGregorianDate = currentDate.format("YYYY-MM-DD");
 	var tempGregorianDate;
 	var tempNepaliDate;
-	
-	console.log(tempFormattedGregorianDate);
+	var dateList = new Array();
 
 	tempNepaliDate = AD2BS(tempFormattedGregorianDate);
 	selectedNepaliDate = tempNepaliDate;
-	console.log(selectedNepaliDate);
-	
-	console.log(tempGregorianDate);
+	console.log("Nepali Date: "+selectedNepaliDate);
+
+	tempNepaliDate =moment(tempNepaliDate).set('date',1).format("YYYY-MM-DD");
+
+	tempGregorianDate = BS2AD(tempNepaliDate);
+	console.log("Temp Gregorian Date: "+tempGregorianDate);
+
+	var currentMonth = moment(tempNepaliDate).get('month');
+	var currentYear = moment(tempNepaliDate).get('year');
+	console.log(currentYear, currentMonth);
+
+	weekday = moment(tempGregorianDate).weekday();
+	diff = moment(tempGregorianDate).subtract(weekday,'days');
+	console.log("diff: "+ moment(diff).format("YYYY-MM-DD"));
+
+	tempGregorianDate = moment(diff).format("YYYY-MM-DD");
+
+	// tempGregorianDate = moment(moment(tempGregorianDate   ).diff(weekday).format("YYYY-MM-DD");
+	console.log((tempGregorianDate));
+
+	tempNepaliDate = AD2BS(tempGregorianDate);
+
+	cond1 = true;
+	cond2 = true;
+	var i = 0;
+	while ((cond1 || cond2) && i++ <=35) {
+
+		// tempGregorianDate = BS2AD(moment(tempNepaliDate).format("YYYY-MM-DD"));
+		 console.log(tempGregorianDate);
+		dateList.push(tempGregorianDate);
+		//console.log(dateList);
+
+		tempGregorianDate = moment(moment(tempGregorianDate).add(1, 'days')).format("YYYY-MM-DD");
+		tempNepaliDate = AD2BS(tempGregorianDate);
+
+		cond1 = (currentYear === moment(tempNepaliDate).get('year') && currentMonth >= moment(tempNepaliDate).get('month'));
+
+		cond2 =  (currentYear > moment(tempNepaliDate).get('year') && currentMonth === 0);
+	}
+	weekday = moment(tempGregorianDate).weekday();
+	// tempGregorianDate = BS2AD(moment(tempNepaliDate).format("YYYY-MM-DD"));
+
+	if(weekday != 0) {
+		for(var i = weekday; i <= 6; i++) {
+			console.log(tempGregorianDate);
+			dateList.push(tempGregorianDate);
+			//console.log(dateList);
+
+			tempGregorianDate = moment(moment(tempGregorianDate).add(1, 'days')).format("YYYY-MM-DD");
+		}
+	}
+
 
 }
