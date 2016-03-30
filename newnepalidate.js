fd8bdfad0e4fd74a7b4f5bc8da93d6a90cac23c7
uvishere <uvishere@gmail.com>
2016-03-30 16:28:12 +0545

    new Design implemented, next and previous months working with some bugs. other month class css implemented

--- newnepalidate.js
+++ newnepalidate.js
@@ -1,30 +1,31 @@
-var setCalendar = function (currentDate) {
-	var tempFormattedGregorianDate = currentDate.format("YYYY-MM-DD");
+//accepts english date only.
+var getCalendarDate = function (currentDate) {
+	var tempFormattedGregorianDate = moment(currentDate).format("YYYY-MM-DD");
 	var tempGregorianDate;
 	var tempNepaliDate;
 	var dateList = new Array();
 
 	tempNepaliDate = AD2BS(tempFormattedGregorianDate);
 	selectedNepaliDate = tempNepaliDate;
-	console.log("Nepali Date: "+selectedNepaliDate);
+	//console.log("Nepali Date: "+selectedNepaliDate);
 
 	tempNepaliDate =moment(tempNepaliDate).set('date',1).format("YYYY-MM-DD");
 
 	tempGregorianDate = BS2AD(tempNepaliDate);
-	console.log("Temp Gregorian Date: "+tempGregorianDate);
+	//console.log("Temp Gregorian Date: "+tempGregorianDate);
 
 	var currentMonth = moment(tempNepaliDate).get('month');
 	var currentYear = moment(tempNepaliDate).get('year');
-	console.log(currentYear, currentMonth);
+	//console.log(currentYear, currentMonth);
 
 	weekday = moment(tempGregorianDate).weekday();
 	diff = moment(tempGregorianDate).subtract(weekday,'days');
-	console.log("diff: "+ moment(diff).format("YYYY-MM-DD"));
+	//console.log("diff: "+ moment(diff).format("YYYY-MM-DD"));
 
 	tempGregorianDate = moment(diff).format("YYYY-MM-DD");
 
 	// tempGregorianDate = moment(moment(tempGregorianDate   ).diff(weekday).format("YYYY-MM-DD");
-	console.log((tempGregorianDate));
+	//console.log((tempGregorianDate));
 
 	tempNepaliDate = AD2BS(tempGregorianDate);
 
@@ -34,9 +35,9 @@ var setCalendar = function (currentDate) {
 	while ((cond1 || cond2) && i++ <=35) {
 
 		// tempGregorianDate = BS2AD(moment(tempNepaliDate).format("YYYY-MM-DD"));
-		 console.log(tempGregorianDate);
+		 //console.log(tempGregorianDate);
 		dateList.push(tempGregorianDate);
-		//console.log(dateList);
+		////console.log(dateList);
 
 		tempGregorianDate = moment(moment(tempGregorianDate).add(1, 'days')).format("YYYY-MM-DD");
 		tempNepaliDate = AD2BS(tempGregorianDate);
@@ -45,18 +46,66 @@ var setCalendar = function (currentDate) {
 
 		cond2 =  (currentYear > moment(tempNepaliDate).get('year') && currentMonth === 0);
 	}
+	
 	weekday = moment(tempGregorianDate).weekday();
 	// tempGregorianDate = BS2AD(moment(tempNepaliDate).format("YYYY-MM-DD"));
 
 	if(weekday != 0) {
 		for(var i = weekday; i <= 6; i++) {
-			console.log(tempGregorianDate);
+			//console.log(tempGregorianDate);
 			dateList.push(tempGregorianDate);
-			//console.log(dateList);
+			////console.log(dateList);
 
 			tempGregorianDate = moment(moment(tempGregorianDate).add(1, 'days')).format("YYYY-MM-DD");
 		}
 	}
+
+	return dateList;
+}
+
+var setCalendar = function(date) {
+
+	var dateList = date;
+	var count = 0;
+ 	var enDateArray = new Array();
+ 	var npDateArray = new Array();
+	var npMonthArray = ["Baisakh","Jestha","Ashad","Shrawan","Bhadra","Ashwin","Kartik","Mangsir","Poush","Magh","Falgun","Chaitra"];
+
+ 	for (var i = 0; i < dateList.length; i++) {
+
+ 		var tempNpDate = AD2BS(moment(dateList[i]).format("YYYY-MM-DD"));
+		var isSameMonth = moment(moment(dateList[i]).format("YYYY-MM-DD")).isSame(moment(dateList[i]).month(),'month');
+ 		// console.log("this month:"+isSameMonth);
+ 		npDateArray.push(moment(tempNpDate).date());
+		enDateArray.push(moment(dateList[i]).date());
+		// console.log(npDateArray);
+	};
+
+	$("#calendar ul li").remove();
+	var bool = false;
+	for(var i = 0; i <= 4 ; i++) {
+	  var ul = $("<ul>").appendTo("#calendar").addClass("days");
+	  for(var j = 0; j <= 6; j++) {
+
+	  	if(npDateArray[count] === 1)
+	  		if(bool)
+	  			bool = false;
+	  		else
+	  			bool = true;
+
+	    	li = $("<li>").appendTo(ul).addClass("day");
+	    	
+	  	if(!bool)
+	    	li = li.addClass("other-month");
+
+	      $("<div>").prepend(npDateArray[count]).appendTo(li).addClass("date");
+	      $("<div>").prepend(enDateArray[count]).appendTo(li).addClass("endate");
+
+	      count = count + 1;
+
+	  };
+
+};
 
 
 }
