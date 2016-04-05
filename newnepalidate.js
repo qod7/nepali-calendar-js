26d60ebd89c3542c19483a4a8c96438459154231
uvishere <uvishere@gmail.com>
2016-04-05 17:21:55 +0545

    fixed prev and next buttons

--- newnepalidate.js
+++ newnepalidate.js
@@ -1,20 +1,64 @@
-//accepts english date only.
+var setDateToOne = function (date) {
+	var tempDate = date.split("-");
+	tempDate[2] =''+0+ "1";
+	tempDate = tempDate.join("-");
+	return tempDate;
+}
+
+var addToMonth = function (date, monthsToAdd) {
+//accepts nepali date
+//adds or subtracts 1 in the given nepali date and returns the new date string
+
+	var tempDate = date.split("-");
+
+	//check if the date is baishakh and previous month is needed and decrease the year value by 1
+	if(tempDate[1] == '01' && monthsToAdd < 0 ){
+		tempDate[0] = parseInt(tempDate[0]) - 1;
+		tempDate[1] = '12';
+	}
+	//check if the date is chaitra and next month is needed and increase the year value by 1
+	else if(tempDate[1] == '12' && monthsToAdd > 0 ){
+		tempDate[0] = parseInt(tempDate[0]) + 1;
+		tempDate[1] = 01;
+	}
+	else {
+	tempDate[1] = parseInt(tempDate[1]) + monthsToAdd;
+	if(tempDate[1] < 10)
+		tempDate[1] = ""+ 0 + tempDate[1];
+	}
+
+	tempDate = tempDate.join("-");
+	return tempDate;
+}
+
+var getCurrentMonth = function (date) {
+	var tempDate = date.split("-");
+	console.log("getCurrentMonth: "+ parseInt(tempDate[1])-1);
+	// return parseToInt(tempDate[1])-1;
+}
+
 var getCalendarDate = function (currentDate) {
-	var tempFormattedGregorianDate = moment(currentDate).format("YYYY-MM-DD");
+//accepts english date only.
+	console.log("currentDate: "+currentDate);
+	var tempFormattedGregorianDate = currentDate;
+	console.log("tempGregorianDate: "+tempFormattedGregorianDate);
 	var tempGregorianDate;
 	var tempNepaliDate;
 	var dateList = new Array();
 
 	tempNepaliDate = AD2BS(tempFormattedGregorianDate);
 	selectedNepaliDate = tempNepaliDate;
-	//console.log("Nepali Date: "+selectedNepaliDate);
+	tempNepaliDate = setDateToOne(tempNepaliDate);
 
-	tempNepaliDate =moment(tempNepaliDate).set('date',1).format("YYYY-MM-DD");
+	console.log("tempNepaliDate: "+tempNepaliDate);
+
+	console.log("tempNepaliDate: "+tempNepaliDate);
 
 	tempGregorianDate = BS2AD(tempNepaliDate);
-	//console.log("Temp Gregorian Date: "+tempGregorianDate);
 
-	var currentMonth = moment(tempNepaliDate).get('month');
+	var currentMonth = moment(tempNepaliDate).month();
+	getCurrentMonth(tempNepaliDate);
+	console.log("currentMonth: "+currentMonth);
 	var currentYear = moment(tempNepaliDate).get('year');
 	//console.log(currentYear, currentMonth);
 
@@ -74,7 +118,6 @@ var setCalendar = function(date) {
  	for (var i = 0; i < dateList.length; i++) {
 
  		var tempNpDate = AD2BS(moment(dateList[i]).format("YYYY-MM-DD"));
-		var isSameMonth = moment(moment(dateList[i]).format("YYYY-MM-DD")).isSame(moment(dateList[i]).month(),'month');
  		// console.log("this month:"+isSameMonth);
  		npDateArray.push(moment(tempNpDate).date());
 		enDateArray.push(moment(dateList[i]).date());
@@ -123,3 +166,40 @@ var setDateTitle = function(date) {
 	console.log(npMonthArray[month]);
 	$(".month-year-name").html(npMonthArray[month]+" "+year);
 }
+
+$(".arrow-next").click(function() {
+
+	tempEngDate = moment(standardDate).format("YYYY-MM-DD");
+	console.log("cuttent english date: "+tempEngDate);
+
+	tempNepDate = AD2BS(tempEngDate);
+	console.log("converted nepal date: "+tempNepDate);
+
+	tempNepDate = addToMonth(tempNepDate,1);
+	console.log("added Nepali Date: " + tempNepDate);
+	standardDate = BS2AD(tempNepDate);
+	//console.log(standardDate);
+	// console.log(standardDate);
+ 	var dateList = getCalendarDate(standardDate);
+
+ 	setDateTitle(standardDate);
+	setCalendar(dateList);
+});
+
+$(".arrow-prev").click(function() {
+
+	tempEngDate = moment(standardDate).format("YYYY-MM-DD");
+	console.log("cuttent english date: "+tempEngDate);
+
+	tempNepDate = AD2BS(tempEngDate);
+	console.log("converted nepal date: "+tempNepDate);
+	tempNepDate = addToMonth(tempNepDate,-1);
+	console.log("subtracted Nepali Date: " + tempNepDate);
+	standardDate = BS2AD(tempNepDate);
+	//console.log(standardDate);
+	// console.log(standardDate);
+ 	var dateList = getCalendarDate(standardDate);
+
+ 	setDateTitle(standardDate);
+	setCalendar(dateList);
+});
